f8b25e56b2397eee4034664583dcda497ea1858c
Fletcher Gornick <fletcher@gornick.dev>
2025-12-01 09:04:18 -0600

    mujoco 3.4.0 -> 3.3.7

--- build.sh
+++ build.sh
@@ -1,26 +1,24 @@
 #!/bin/sh
 
 cd "$(dirname "$0")"
-
 OS="$(uname -s)"
 ARCH="$(uname -m)"
 
 if [ "$OS" = "Darwin" ] && [ "$ARCH" = "arm64" ]; then
     clang++ -std=c++17 -O3 -Wall \
         -Iinc \
+        -Llib_arm64_darwin \
         src/main.cpp -o muludnep \
-        lib_arm64_darwin/* \
-        -framework Cocoa \
-        -framework IOKit \
-        -framework OpenGL \
-        -Wl,-rpath,./lib_arm64_darwin
+        -Wl,-rpath,./lib_arm64_darwin \
+        -framework Cocoa -framework IOKit -framework OpenGL \
+        -lmujoco -limgui -lglfw3
 elif [ "$OS" = "Linux" ] && [ "$ARCH" = "x86_64" ]; then
     g++ -std=c++17 -O3 -Wall \
         -Iinc \
+        -Llib_x64_linux \
         src/main.cpp -o muludnep \
-        lib_x64_linux/* \
-        -lGL \
-        -Wl,-rpath,./lib_x64_linux
+        -Wl,-rpath,./lib_x64_linux \
+        -lGL -lmujoco -limgui -lglfw3
 else
     echo "unsupported platform: $OS ($ARCH)"
     exit 1
