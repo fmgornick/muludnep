368693329755d8c6bc5bb2430d3e38130f4b57f6
Fletcher Gornick <fletcher@gornick.dev>
2025-11-30 22:04:08 -0600

    static mujoco lib

--- build.sh
+++ build.sh
@@ -2,11 +2,19 @@
 
 cd "$(dirname "$0")"
 
-clang++ -std=c++17 -O3 -Wall \
-    -Flibs/mujoco \
-    -Iinc \
-    -Llib_darwin \
-    -Wl,-rpath,$dir/libs/mujoco \
-    src/main.cpp -o muludnep \
-    -lglfw -limgui \
-    -framework mujoco -framework OpenGL
+OS="$(uname -s)"
+ARCH="$(uname -m)"
+
+if [ "$OS" = "Darwin" ] && [ "$ARCH" = "arm64" ]; then
+    clang++ -std=c++17 -O3 -Wall \
+        -Iinc \
+        src/main.cpp -o muludnep \
+        lib_darwin/*.a \
+        -framework Cocoa \
+        -framework IOKit \
+        -framework OpenGL
+# elif [ "$OS" = "Linux" ] && [ "$ARCH" = "x86_64" ]; then
+else
+    echo "Unsupported platform: $OS ($ARCH)"
+    exit 1
+fi
